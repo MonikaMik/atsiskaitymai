5c9326949272425b5202b9fd2906136f069f6e29
MonikaMik <monikacibirauskaite@yahoo.com>
2024-02-14 09:32:03 +0200

    4 uzd

--- Javascript_Atsiskaitymas/Task 4/script.js
+++ Javascript_Atsiskaitymas/Task 4/script.js
@@ -10,3 +10,28 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+fetch(ENDPOINT)
+    .then(res => res.json())
+    .then(cars => {
+        const outputDiv = document.querySelector('#output');
+
+        cars.forEach(car => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            
+            const brandTitle = document.createElement('h2');
+            brandTitle.textContent = car.brand;
+            
+            const modelsList = document.createElement('ul');
+            car.models.forEach(model => {
+                const modelItem = document.createElement('li');
+                modelItem.textContent = model;
+                modelsList.appendChild(modelItem);
+            });
+            card.append(brandTitle, modelsList);
+            
+            outputDiv.appendChild(card);
+        })
+    })
+        .catch(error => console.error(error))
